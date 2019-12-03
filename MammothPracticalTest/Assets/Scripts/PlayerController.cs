d4f3437f1f0cc5521029810cfa401c688c7a11ea
daclare24 <58451771+daclare24@users.noreply.github.com>
2019-12-02 18:54:05 -0700

    First Upload

---
+++ MammothPracticalTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float cameraSensitivity = 2.0f;
+    public float moveSensitivity = 0.5f;
+    public float ballSpeed = 10.0f;
+    public float fireRate = 0.3f;
+    public GameObject ball;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    private bool armed = true;
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"), Input.GetAxis("Vertical")) * moveSensitivity;
+        transform.Translate(move, Space.Self);
+
+        yaw += cameraSensitivity * Input.GetAxis("Mouse X");
+        pitch -= cameraSensitivity * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        if (Input.GetButton("Cancel"))
+            Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (armed)
+        {
+            if (Input.GetButtonDown("Fire1") || Input.GetButton("Fire2"))
+            {
+                Fire();
+                armed = false;
+                StartCoroutine(rearm());
+            }
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject newBall = Instantiate(ball);
+        newBall.transform.position = transform.position + transform.forward * 1.5f + transform.up * -1.0f;
+        newBall.GetComponent<Rigidbody>().velocity = transform.forward * ballSpeed;
+    }
+
+    private IEnumerator rearm()
+    {
+        yield return new WaitForSeconds(fireRate);
+        armed = true;
+    }
+
+}
---
+++ MammothPracticalTest/Assets/Scripts/RingControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingControl : MonoBehaviour
+{
+    public float movementRadius = 5.0f;
+    public float moveSpeed = 60.0f;
+    public Color hitColour;
+    public bool Clockwise;
+
+    private Vector3 origin;
+    private int direction;
+    private Color originalColour;
+    private float hitTime = 2.0f;
+    private bool hit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Clockwise)
+            direction = 1;
+        else
+            direction = -1;
+
+        origin = transform.position;
+        originalColour = GetComponent<Renderer>().material.color;
+
+        transform.position = new Vector3(transform.position.x,movementRadius,transform.position.z);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.RotateAround(origin, new Vector3(0.0f, 0.0f, 1.0f * direction), moveSpeed * Time.deltaTime);
+        if (transform.position.y < origin.y)
+        {
+            direction *= -1;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!hit)
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+            hit = true;
+            StartCoroutine(revertColour());
+        }
+    }
+
+    private IEnumerator revertColour()
+    {
+        yield return new WaitForSeconds(hitTime);
+        GetComponent<Renderer>().material.SetColor("_Color", originalColour);
+        hit = false;
+    }
+}
